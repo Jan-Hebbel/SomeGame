cad7ddfeeb13a942cc2a68e1113e7254e6cb94ba
Jan Hebbel Aguiar <jan.hebbel@gmail.com>
2023-05-28 18:35:26 +0200

    added comment

--- src/input.hpp
+++ src/input.hpp
@@ -1,3 +1,21 @@
+/*
+* Here is how input works for this game:
+* 
+* Once per frame platform_process_events() is called. This function
+* polls Windows for events. If the event was a Keydown or 
+* Syskeydown event we set the local keyboard state for the key to 
+* true. Once the PeekMessage-Loop is done, we call the 
+* input_add_events() function to add events to event_queue for all 
+* keys that got set to true in keyboard_state in that PeekMessage-
+* Loop. 
+* 
+* Later in the game_update() function we empty out the queue that
+* holds all events that got added for this frame by calling 
+* event_queue_next(). This way we keep track of the state of all
+* keys that are down for one frame and handle it later in the 
+* game_update() function.
+*/
+
 #ifndef INPUT_H
 #define INPUT_H
 
