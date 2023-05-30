22fcd200a5e0bd60da21af82fe90c333e605250f
Jan Hebbel Aguiar <jan.hebbel@gmail.com>
2023-05-30 17:18:16 +0200

    Now correctly doing transparency and blending.

--- src/renderer/vulkan_init.cpp
+++ src/renderer/vulkan_init.cpp
@@ -1429,12 +1429,12 @@ bool32 renderer_vulkan_init() {
 		};
 
 		VkPipelineColorBlendAttachmentState color_blend_attachment{
-			.blendEnable = VK_FALSE,
+			.blendEnable = VK_TRUE,
 			.srcColorBlendFactor = VK_BLEND_FACTOR_SRC_ALPHA,
 			.dstColorBlendFactor = VK_BLEND_FACTOR_ONE_MINUS_SRC_ALPHA,
 			.colorBlendOp = VK_BLEND_OP_ADD,
-			.srcAlphaBlendFactor = VK_BLEND_FACTOR_ONE,
-			.dstAlphaBlendFactor = VK_BLEND_FACTOR_ZERO,
+			.srcAlphaBlendFactor = VK_BLEND_FACTOR_SRC_ALPHA,
+			.dstAlphaBlendFactor = VK_BLEND_FACTOR_ONE_MINUS_SRC_ALPHA,
 			.alphaBlendOp = VK_BLEND_OP_ADD,
 			.colorWriteMask = VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT | VK_COLOR_COMPONENT_B_BIT | VK_COLOR_COMPONENT_A_BIT,
 		};
